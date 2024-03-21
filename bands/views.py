88b5d7573a0daf576e5c57f6905d083909391740
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-03-21 04:16:24 -0500

    update musicians view, template with paginator

--- bands/views.py
+++ bands/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from bands.models import Musician
 
 def musician(request, musician_id):
+
     musician = get_object_or_404(Musician, id=musician_id)
 
     data = {
@@ -11,10 +13,24 @@ def musician(request, musician_id):
     return render(request, "musician.html", data) 
 
 def musicians(request):
-    musicians = Musician.objects.all().order_by("last_name")
+
+    all_musicians = Musician.objects.all().order_by("last_name")
+
+    paginator = Paginator(all_musicians, 2)
+
+    page_num = request.GET.get('page', 1)
+    page_num = int(page_num)
+
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+    
+    page = paginator.page(page_num)
 
     data = {
-        "musicians": musicians
+        "musicians": page.object_list,
+        "page": page,
     }
 
     return render(request, "musicians.html", data)
