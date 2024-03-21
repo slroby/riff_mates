21adfc49f539c4f39d1b77a4d652d096736d8eb3
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-03-21 03:55:34 -0500

    add musicians view, url, template

--- bands/views.py
+++ bands/views.py
@@ -10,5 +10,15 @@ def musician(request, musician_id):
 
     return render(request, "musician.html", data) 
 
+def musicians(request):
+    musicians = Musician.objects.all().order_by("last_name")
+
+    data = {
+        "musicians": musicians
+    }
+
+    return render(request, "musicians.html", data)
+
 print("http://127.0.0.1:8000/bands/musician/1")
+print("http://127.0.0.1:8000/bands/musicians/")
 
