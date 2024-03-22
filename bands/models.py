e4f9eb3d6558f8466f0c1ac46ebc80abb4d7cebc
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-03-21 23:52:16 -0500

    update models

--- bands/models.py
+++ bands/models.py
@@ -7,3 +7,16 @@ class Musician(models.Model):
 
     def __str__(self):
         return f"Musician(id={self.id}, last_name={self.last_name})"
+
+class Venue(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Venue(id={self.id}, name={self.name})"
+    
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    venue = models.ForeignKey(Venue, onDelete=models.CASCADE)
+
+    def __str__(self):
+        return f"Room(id={self.id}, name={self.name}, venue={self.venue})"
