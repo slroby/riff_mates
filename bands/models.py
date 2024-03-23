826e894630cf22ee297e76f34cceeec680ebf150
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-03-23 03:44:46 -0500

    update models

--- bands/models.py
+++ bands/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -8,18 +9,21 @@ class Musician(models.Model):
     def __str__(self):
         return f"Musician(id={self.id}, last_name={self.last_name})"
 
+
 class Venue(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"Venue(id={self.id}, name={self.name})"
-    
+
+
 class Room(models.Model):
     name = models.CharField(max_length=50)
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Room(id={self.id}, name={self.name})"
+
 
 class Band(models.Model):
     name = models.CharField(max_length=50)
