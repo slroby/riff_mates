887ba12d986059b25bcc3a5a9e0bb647a81476a7
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-03-31 08:33:29 -0500

    add band and bands tamplates, views, urls

--- bands/urls.py
+++ bands/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from bands import views
 
 urlpatterns = [
-  path('musician/<int:musician_id>/', views.musician, name="musician"), 
-  path('musicians/', views.musicians, name="musicians"),
+    path("musician/<int:musician_id>/", views.musician, name="musician"),
+    path("musicians/", views.musicians, name="musicians"),
+    path("band/<int:band_id>/", views.band, name="band"),
+    path("bands/", views.bands, name="bands"),
 ]
--- bands/views.py
+++ bands/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from bands.models import Musician
+from bands.models import Band
 
 
 def musician(request, musician_id):
@@ -43,5 +44,50 @@ def musicians(request):
     return render(request, "musicians.html", data)
 
 
+def band(request, band_id):
+
+    band = get_object_or_404(Band, id=band_id)
+    print(band)
+
+    data = {
+        "band": band,
+    }
+
+    return render(request, "band.html", data)
+
+
+def bands(request):
+
+    print(request.GET)
+
+    all_bands = Band.objects.all().order_by("name")
+
+    num_objects = request.GET.get("num", 2)
+
+    if int(num_objects) > 10:
+        num_objects = 10
+
+    paginator = Paginator(all_bands, num_objects)
+
+    page_num = request.GET.get("page", 1)
+    page_num = int(page_num)
+
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+
+    data = {
+        "bands": page.object_list,
+        "page": page,
+    }
+
+    return render(request, "bands.html", data)
+
+
 print("http://127.0.0.1:8000/bands/musician/1")
 print("http://127.0.0.1:8000/bands/musicians/")
+print("http://127.0.0.1:8000/bands/band/1")
+print("http://127.0.0.1:8000/bands/bands/")
