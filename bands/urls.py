5844f68bbe727c08cce4094937b43bb63f36f59e
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-04-02 09:29:09 -0500

    add venues/venue/room urls, views, templates

--- bands/urls.py
+++ bands/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("musicians/", views.musicians, name="musicians"),
     path("band/<int:band_id>/", views.band, name="band"),
     path("bands/", views.bands, name="bands"),
+    path("room/<int:room_id>/", views.room, name="room"),
+    path("venue/<int:venue_id>/", views.venue, name="venue"),
+    path("venues/", views.venues, name="venues"),
 ]
--- bands/views.py
+++ bands/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from bands.models import Musician
-from bands.models import Band
+from bands.models import Musician, Band, Room, Venue
 
 
 def musician(request, musician_id):
@@ -91,7 +90,70 @@ def bands(request):
     return render(request, "bands.html", data)
 
 
+def room(request, room_id):
+
+    room = get_object_or_404(Room, id=room_id)
+    print(room)
+
+    data = {
+        "room": room,
+        "musicians": musicians,
+    }
+
+    return render(request, "room.html", data)
+
+
+def venue(request, venue_id):
+
+    venue = get_object_or_404(Venue, id=venue_id)
+    print(venue)
+
+    rooms = venue.room_set.all()
+    print(rooms)
+
+    data = {
+        "venue": venue,
+        "rooms": rooms,
+    }
+
+    return render(request, "venue.html", data)
+
+
+def venues(request):
+
+    print(request.GET)
+
+    all_venues = Venue.objects.all().order_by("name")
+
+    num_objects = request.GET.get("num", 2)
+
+    if int(num_objects) > 10:
+        num_objects = 10
+
+    paginator = Paginator(all_venues, num_objects)
+
+    page_num = request.GET.get("page", 1)
+    page_num = int(page_num)
+
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+
+    data = {
+        "venues": page.object_list,
+        "page": page,
+    }
+
+    return render(request, "venues.html", data)
+
+
 print("http://127.0.0.1:8000/bands/musician/1")
 print("http://127.0.0.1:8000/bands/musicians/")
 print("http://127.0.0.1:8000/bands/band/1")
 print("http://127.0.0.1:8000/bands/bands/")
+print("http://127.0.0.1:8000/bands/room/1")
+print("http://127.0.0.1:8000/bands/venue/1")
+print("http://127.0.0.1:8000/bands/venues/")
