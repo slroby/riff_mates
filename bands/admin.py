1f621e2f529859e2caa051f667b1b617e9c62f4a
Sean Roby <80441248+slroby@users.noreply.github.com>
2024-04-09 18:30:52 -0500

    add Band, Venue, Room admin

--- bands/admin.py
+++ bands/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from bands.models import Musician
+from bands.models import Musician, Band, Venue, Room
 
 # Register your models here.
 
 
 @admin.register(Musician)
 class MusicianAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
     pass
